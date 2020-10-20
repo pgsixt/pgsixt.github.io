d8f8a14c6d1401da73321858b9f2fb0e63af1561
pgsixt <65584222+pgsixt@users.noreply.github.com>
2020-10-20 23:58:43 +0900

    js를 이용한 교회별데이터 준비

---
+++ prayer_list.js
@@ -0,0 +1,3 @@
+var prayer_list={"xEfa":{"churchname":"플라워마운드교회", edited: [{"slideNum":2, title:"[아니], [[이럴 수가]]", prayer:"김 XX 권사님 건강위해서 기도합시다. 나 하늘에 맹세해"},{"slideNum":9, prayer:"미국사회"}] }};
+
+prayer_list["xEfa2"]={"churchname":"은혜로운교회", edited: [{"slideNum":3, prayer:"김 XX 학생 수능시험위해"},{"slideNum":4, prayer:"미국사회"}]};
