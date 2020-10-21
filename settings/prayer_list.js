4ea92dcc2402eaa89a6505cdf7a57b39521a9bcd
pgsixt <65584222+pgsixt@users.noreply.github.com>
2020-10-21 21:53:03 +0900

    Create prayer_list.js

---
+++ settings/prayer_list.js
@@ -0,0 +1,9 @@
+var prayer_list={"xEfa":{"churchname":"플라워마운드교회", "timestamp":"2020-10-21 11:22:33", "roomnumber":"6","edited": [{"slideNum":2, prayer:"제목::[얼굴을], [[무릎 사이에 넣고]]\n===\n설명*::말씀을 새기며 기도합시다.\n설명*::우리가 원하는 것을 나열한들 주님과 상관없으면 아무것도 아닙니다.\n---큰본문::우리는 [많은 말]을 해야 좋은 기도라고 생각합니다.\n줄바꿈::\n큰본문::우리가 기도할 내용보다 주님께서 하실 말씀이 더욱 많습니다. 기도를 통해 주님의 뜻을 듣게 되길 기도합시다.\n줄바꿈::\n큰본문::주님 우리가 먼저 [듣기] 원하오니, 이 기도의 시간이 듣는 자리가 되게 하소서\n줄바꿈::\n다음버튼::"},{"slideNum":9, prayer:"미국사회"}] }};
+
+prayer_list["xEfa2"]={"churchname":"플라워마운드교회", "timestamp":"2020-10-21 11:22:33", "roomnumber":"6","edited": [{"slideNum":2, prayer:"<h4>김 XX 권사님 건강위해서 기도합시다. 나 하늘에 맹세해</h4>"},{"slideNum":9, prayer:"미국사회"}] };
+
+ 
+
+
+
+//prayer_list["xEfa2"]={"churchname":"은혜로운교회", edited: [{"slideNum":3, prayer:"김 XX 학생 수능시험위해"},{"slideNum":4, prayer:"미국사회"}]};
